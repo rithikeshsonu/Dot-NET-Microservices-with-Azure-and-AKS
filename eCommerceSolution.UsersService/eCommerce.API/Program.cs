61fd1fe1e15a9295b1cb1c12a4a1bef7ccd195a6
Rithikesh Surthi <rithikeshsonu13@gmail.com>
2025-03-05 23:31:55 +0530

    Middlewares

--- eCommerceSolution.UsersService/eCommerce.API/Program.cs
+++ eCommerceSolution.UsersService/eCommerce.API/Program.cs
@@ -1,6 +1,29 @@
+using eCommerce.Infrastructure;
+using eCommerce.Core;
+using eCommerce.API.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddInfrastructure();
+builder.Services.AddCore();
+
+//Add controllers to the service collection
+builder.Services.AddControllers();
+
+//Build the web app
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseExceptionHandlingMiddleware();
+
+//Routing
+app.UseRouting();
+
+//Auth
+app.UseAuthentication();
+app.UseAuthorization();
+
+//Controller routes
+app.MapControllers();
+
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
