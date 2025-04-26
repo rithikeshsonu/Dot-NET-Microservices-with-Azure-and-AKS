e5fc46cdba66146aff3b978fc603932b8d1f6c01
Rithikesh Surthi <rithikeshsonu13@gmail.com>
2025-04-27 03:52:42 +0530

    Dapper

--- eCommerceSolution.UsersService/eCommerce.Infrastructure/Repositories/UsersRepository.cs
+++ eCommerceSolution.UsersService/eCommerce.Infrastructure/Repositories/UsersRepository.cs
@@ -1,32 +1,36 @@
-﻿using eCommerce.Core.DTO;
+﻿using Dapper;
+using eCommerce.Core.DTO;
 using eCommerce.Core.Entities;
 using eCommerce.Core.RepositoryContracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using eCommerce.Infrastructure.DbContext;
 
 namespace eCommerce.Infrastructure.Repositories
 {
     internal class UsersRepository : IUsersRepository
     {
+        private readonly DapperDbContext _dbContext;
+        public UsersRepository(DapperDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public async Task<ApplicationUser?> AddUser(ApplicationUser user)
         {
             user.UserId = Guid.NewGuid();
-            return user;
+            string query = "INSERT INTO public.\"Users\"(\"UserId\", \"Email\",\"PersonName\",\"Gender\",\"Password\") VALUES (@UserId, @Email, @PersonName, @Gender, @Password)";
+            int affectedRowsCount = await _dbContext.DbConnection.ExecuteAsync(query, user);
+            if(affectedRowsCount > 0)
+            {
+                return user;
+            }
+            return null;
         }
 
         public async Task<ApplicationUser?> GetUserByEmailAndPassword(string? email, string? password)
         {
-            return new ApplicationUser()
-            {
-                Email = email,
-                Password = password,
-                UserId = Guid.NewGuid(),
-                PersonName = "Person Name",
-                Gender = GenderOptions.Female.ToString(),
-            };
+            string query = "SELECT * FROM public.\"Users\" WHERE \"Email\" = @Email AND \"Password\" = @Password";
+            var parameters = new {Email = email, Password = password};
+            ApplicationUser? user = await _dbContext.DbConnection.QueryFirstOrDefaultAsync<ApplicationUser>(query, parameters);
+            return user;
         }
     }
 }
