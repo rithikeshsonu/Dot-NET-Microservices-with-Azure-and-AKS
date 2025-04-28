4e2400248a09aef7b84f9cde4de19995ca0d5d24
Rithikesh Surthi <rithikeshsonu13@gmail.com>
2025-04-28 21:12:32 +0530

    Fluent Validations

---
+++ eCommerceSolution.ProductsService/BusinessLogicLayer/Validators/ProductAddRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using eCommerce.BusinessLogicLayer.DTO;
+using FluentValidation;
+
+namespace eCommerce.BusinessLogicLayer.Validators
+{
+    public class ProductAddRequestValidator : AbstractValidator<ProductAddRequest>
+    {
+        public ProductAddRequestValidator()
+        {
+            RuleFor(temp => temp.ProductName).NotEmpty().WithMessage("Product Name can't be blank");
+            RuleFor(temp => temp.Category).IsInEnum().WithMessage("Category should be provided");
+            RuleFor(temp => temp.UnitPrice).InclusiveBetween(0, double.MaxValue).WithMessage($"Unit price should be between 0 and {double.MaxValue}");
+            RuleFor(temp => temp.QuantityInStock).InclusiveBetween(0, int.MaxValue).WithMessage($"Quantity should between 0 and {int.MaxValue}");
+        }
+    }
+}
---
+++ eCommerceSolution.ProductsService/BusinessLogicLayer/Validators/ProductUpdateRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using eCommerce.BusinessLogicLayer.DTO;
+using FluentValidation;
+
+namespace eCommerce.BusinessLogicLayer.Validators
+{
+    public class ProductUpdateRequestValidator : AbstractValidator<ProductUpdateRequest>
+    {
+        public ProductUpdateRequestValidator()
+        {
+            RuleFor(temp => temp.ProductId).NotEmpty().WithMessage("Product ID can't be blank");
+            RuleFor(temp => temp.ProductName).NotEmpty().WithMessage("Product Name can't be blank");
+            RuleFor(temp => temp.Category).IsInEnum().WithMessage("Category should be provided");
+            RuleFor(temp => temp.UnitPrice).InclusiveBetween(0, double.MaxValue).WithMessage($"Unit price should be between 0 and {double.MaxValue}");
+            RuleFor(temp => temp.QuantityInStock).InclusiveBetween(0, int.MaxValue).WithMessage($"Quantity should between 0 and {int.MaxValue}");
+        }
+    }
+}
