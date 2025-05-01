00bf4a53d2713cc25a6389ed273b5dcaeb731119
Rithikesh Surthi <rithikeshsonu13@gmail.com>
2025-05-01 21:41:51 +0530

    Add Swagger and Cors policy

--- eCommerceSolution.ProductsService/ProducrsService.API/Program.cs
+++ eCommerceSolution.ProductsService/ProducrsService.API/Program.cs
@@ -3,6 +3,7 @@ using eCommerce.ProductsService.BusinessLogicLayer;
 using eCommerce.ProductsService.DataAccessLayer;
 using FluentValidation.AspNetCore;
 using ProductsService.API.Middlewares;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 //Add Business Logic Layer and DAL
@@ -12,15 +13,37 @@ builder.Services.AddDataAccessLayer(builder.Configuration);
 builder.Services.AddControllers();
 
 builder.Services.AddFluentValidationAutoValidation();
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy(builder => {
+        builder.WithOrigins("http://localhost:4200")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
 app.UseExceptionHandlingMiddleware();
 app.UseRouting();
 
+app.UseCors();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 //Auth
-app.UseAuthorization();
 app.UseAuthentication();
+app.UseHttpsRedirection();
+app.UseAuthorization();
 
 app.MapControllers();
 app.MapProductAPIEndpoints();
